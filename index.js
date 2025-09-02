e9b3fa68d12d565a15008c81c99b65bb12f13e42
gautams180 <sahugautam944@gmail.com>
2025-09-02 21:42:20 +0530

    added ca path for aiven database

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const ENV = process.env;
 const compression = require("compression");
 // const morgan = require("morgan");
 // require("./cron/cron");
+app.use("/v1", v1);
 app.use(express.static(__dirname + ENV.CLIENT));
 const html = ENV.CLIENT;
 // const { socketInit } = require('./services/sockets/socket');
@@ -57,8 +58,6 @@ app.use(cors());
 // app.options("*", cors());
 
 const v1 = require("./apis/versions/v1");
-
-app.use("/v1", v1);
 
 server.listen(ENV.PORT, '0.0.0.0', () => {
   logger.info(`⚙️  Server started on ${ENV.URL}:${ENV.PORT}`);
